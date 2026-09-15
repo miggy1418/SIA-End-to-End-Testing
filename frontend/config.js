d32ae7f4164569442e31b137fb419cf84a5396bd
miggy1418 <miggy.tademuwu@gmail.com>
2026-09-15 08:07:25 +0800

    Add files via upload

---
+++ frontend/config.js
@@ -0,0 +1,6 @@
+// Edit these if you run the services on different hosts/ports.
+const CONFIG = {
+  PRODUCT_SERVICE_URL: "http://localhost:4001",
+  ORDER_SERVICE_URL: "http://localhost:4002",
+  PAYMENT_SERVICE_URL: "http://localhost:4003",
+};
